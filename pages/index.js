82a2aa6d4fe7fb6d06404b5531a8c6ae8fa46417
sksprades <sksprades.student@ua.edu.ph>
2026-09-22 11:39:14 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <section className={utilStyles.headingMd}>
+        <p>10 Things That Requires Zero Talent</p>
+
+          <ul>
+            <li>Being On Time</li>
+            <li>Making An Effort</li>
+            <li>Being High Energy</li>
+            <li>Having A Positive Attitude</li>
+            <li>Being Passionate</li>
+            <li>Using Good Body Language</li>
+            <li>Being Coachable</li>
+            <li>Doing A Little Extra</li>
+            <li>Being Prepared</li>
+            <li>Having A Strong Work Ethic</li>
+          </ul>
+          
+          
+          
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
